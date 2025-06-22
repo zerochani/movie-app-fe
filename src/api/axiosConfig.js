4297b14eb97fa1f2ca1aaac8867ae4d2cb20f8ea
zerochani <youngchan0510@gmail.com>
2025-06-22 22:30:05 +0900

    axiosConfig.js 수정

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 // 프록시를 사용하므로 baseURL을 특정할 필요가 없습니다.
 // 모든 요청은 package.json의 프록시 설정을 따르게 됩니다.
 const apiClient = axios.create({
+    baseURL: process.env.REACT_APP_API_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     }
